57a59c63f9066f5e92bd4150208a863bbd422775
Moatasem-x <moatasememad55555@gmail.com>
2025-07-07 23:32:50 +0300

    7/7 11:30

--- src/app/Account/register/register.ts
+++ src/app/Account/register/register.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,11 +10,13 @@ import { RouterLink } from '@angular/router';
 })
 export class Register {
 
+  constructor(private router: Router) {}
+
   registerForm = new FormGroup({
     "name": new FormControl("", [Validators.required]),
     "email": new FormControl("", [Validators.required, Validators.email]),
     "address": new FormControl("", [Validators.required]),
-    "password": new FormControl("", [Validators.required]),
+    "password": new FormControl("", [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)]),
     "confirmPassword": new FormControl("", [Validators.required])
   });
 
@@ -42,6 +44,7 @@ export class Register {
     if(this.registerForm.status == "VALID" && this.getPassword().value == this.getConfirmPassword().value)
     {
       console.log(this.registerForm.value);
+      this.router.navigateByUrl("/login");
     }
   }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Home } from './Pages/home/home';
 import { Register } from './Account/register/register';
 import { Login } from './Account/login/login';
 import { CurrentExams } from './Pages/current-exams/current-exams';
+import { Dashboard } from './Components/dashboard/dashboard';
+import { AdminDashboard } from './Components/admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
     {path: "", redirectTo: "home", pathMatch: "full"},
     {path:"home", component: Home},
     {path: "register", component: Register},
     {path: "login", component: Login},
-    {path: "exams", component: CurrentExams}
+    {path: "exams", component: CurrentExams},
+    {path: "stdash", component: Dashboard},
+    {path: "admindash", component: AdminDashboard}
 
 ];
